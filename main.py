62603bbc520396c0c9f4317196fca563206cb299
hiroshimashu <hiroshimashu@gmail.com>
2020-12-24 14:56:38 +0900

    add: implementing function to get titles and saving these

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
+import requests
 from bs4 import BeautifulSoup
 
-url = "https://www/ymori.com/books/python2nen/test1.html"
+url = "https://www.patentlyapple.com/"
 response = requests.get(url)
+soup = BeautifulSoup(response.content, "html.parser")
 
-response.encoding = response.apparent_encoding
+entries = soup.find(class_ = "left-container-inner")
 
-print(response.text)
+filename = "titles.txt"
+with open(filename, "w") as f: 
+    for title_element in entries.find_all(class_ = "entry-title"):
+        title = title_element.find("a").text
+        link = title_element.find("a").get("href")
+        f.write("title: " + title + "\n")
+        f.write("link: " + link + "\n") 
+        f.write("\n")
+
+    
